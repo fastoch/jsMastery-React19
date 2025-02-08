71ec9d22ff262464d7a41616e5366cb5d4893bdb
fastoch <fastoch@ik.me>
2025-02-08 11:03:16 +0100

    @87 MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const MovieCard = ({ key, movie }) => {
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
   return (
-    <div>
-      <p key={movie.id} className='text-white'>{movie.title}</p>
+    <div className='movie-card'>
+      <img 
+        src={poster_path ? 
+        `https://image.tmdb.org/t/p/w500/${poster_path}` 
+        : '/no-movie.png' }
+        alt={title} 
+      />
+
+      <div className='mt-4'>
+        <h3>{title}</h3>
+      </div>
     </div>
   )
 }
