36e1b40001567c6c0b545e88f8fd8710e0a0a74c
fastoch <fastoch@ik.me>
2025-02-08 17:23:54 +0100

    @90 Implement Search

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,22 @@ const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, or
 
       <div className='mt-4'>
         <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="star icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+          </div>
+
+          <span>⦁</span> {/* just a separator */}
+
+          <p className="lang">{original_language}</p>
+
+          <span>⦁</span>
+
+          <p className="year">
+            {release_date ? release_date.split('-')[0] : 'N/A'}
+          </p>
+        </div>
       </div>
     </div>
   )
